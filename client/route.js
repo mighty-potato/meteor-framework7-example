439009914023166c37433fb414d1ef6106345643
keang hok <laykeanghok@gmail.com>
2017-03-24 23:47:35 +0700

    add animation

--- client/route.js
+++ client/route.js
@@ -1,15 +1,34 @@
 
 //either set root for blaze or set root for framework7
-BlazeLayout.setRoot('body');
+// BlazeLayout.setRoot('body');
 
 FlowRouter.route('/', {
+    name: 'page1',
     action: function (params, queryParams) {
-        BlazeLayout.render('MainLayout', { main: 'Page1' });
+        // BlazeLayout.render('MainLayout', { main: 'Page1' });
+         FlowTransition.flow({body: "Page1"});
     }
 });
 
 FlowRouter.route('/page2', {
+     name: 'page2',
     action: function (params, queryParams) {
-        BlazeLayout.render('MainLayout', { main: 'Page2' });
+        // BlazeLayout.render('MainLayout', { main: 'Page2' });
+          FlowTransition.flow({body: "Page2"});
     }
 })
+
+FlowTransition.addTransition({
+  section: 'body',
+  from: 'page1',
+  to: 'page2',
+  txFull: 'left' // direction of motion, content will be moving left
+});
+
+
+FlowTransition.addTransition({
+  section: 'body',
+  from: 'page2',
+  to: 'page1',
+  txFull: 'right' // direction of motion, content will be moving right
+});

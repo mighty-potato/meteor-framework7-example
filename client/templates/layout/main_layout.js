8f988527ea97cabd1a8bd2eb5c9728e1b15973d2
keang hok <laykeanghok@gmail.com>
2017-06-06 02:31:54 +0700

    small change

--- client/templates/layout/main_layout.js
+++ client/templates/layout/main_layout.js
@@ -1,13 +1,12 @@
 
 
-Template.MainLayout.onCreated(() => {
+Template.MainLayout.onRendered(() => {
   // Initialize app
   let myApp = new Framework7({
-    router: false // prevent framework 7 to override flow router link
+    router: false, // prevent framework 7 to override flow router link
   });
 
   // // If we need to use custom DOM library, let's save it to $$ variable:
   let $$ = Dom7;
 
 });
-
